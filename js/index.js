cdc9fdb9db07f23a3e6f9324da7e3ebb734e3465
Lindauro Neto <netolindauro@gmail.com>
2024-08-24 00:33:17 -0300

    Revert "Quase perfeição das funções de Criptografia e Descriptografia"
    This reverts commit 2cad50db824ad54365f6ae9cc93cc51bb2472cab.

--- js/index.js
+++ js/index.js
@@ -2,55 +2,31 @@ let inputTraducao = document.querySelector('#inputTraducao');
 let btnCript = document.querySelector('#btnCriptografar');
 let btnDescript = document.querySelector('#btnDescriptografar');
 
-let cxResultado = document.querySelector('#cxResultado');
-let imgResult = document.querySelector('#imagemResultado');
-let titResult = document.querySelector('#tituloResultado');
-let txtResult = document.querySelector('#textoResultado');
-
 let txtInput;
-let txtCriptografado;
-let txtDescriptografado;
 
 btnCript.addEventListener('click', function (e) {
     txtInput = inputTraducao.value;
-    
-    cxResultado.classList.remove('resultado');
-    cxResultado.classList.add('resultado__unhidden');
-    imgResult.style.display = 'none';
-    titResult.style.display = 'none';
 
-    txtCriptografado = txtInput
-    .replaceAll('e', 'enter')
-    .replaceAll('a', 'ai')
-    .replaceAll('i', 'imes')
-    .replaceAll('u', 'ufat')
-    .replaceAll('o', 'ober');
+   let txtCriptografado = txtInput
+        .replaceAll('e', 'enter')
+        .replaceAll('a', 'ai')
+        .replaceAll('i', 'imes')
+        .replaceAll('u', 'ufat')
+        .replaceAll('o', 'ober');
 
-    txtResult.textContent = txtCriptografado;
-    inputTraducao.value = ''
     e.preventDefault();
-
     return txtCriptografado;
 })
 
 btnDescript.addEventListener('click', function (e) {
-    txtInput = inputTraducao.value;
 
-    cxResultado.classList.remove('resultado');
-    cxResultado.classList.add('resultado__unhidden');
-    imgResult.style.display = 'none';
-    titResult.style.display = 'none';
+    let txtDescriptografado = txtCriptografado
+        .replaceAll('enter', 'e')
+        .replaceAll('ai', 'i')
+        .replaceAll('imes', 'i')
+        .replaceAll('ufat', 'u')
+        .replaceAll('ober', 'o');
 
-    txtDescriptografado = txtInput
-    .replaceAll('enter', 'e')
-    .replaceAll('ai', 'a')
-    .replaceAll('imes', 'i')
-    .replaceAll('ufat', 'u')
-    .replaceAll('ober', 'o');
-
-    txtResult.textContent = txtDescriptografado;
-    inputTraducao.value = ''
-    e.preventDefault();
-
+    e.preventDefault;
     return txtDescriptografado;
 })
